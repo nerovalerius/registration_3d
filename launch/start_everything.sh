d22c3b1ed3e035e3f3cf9ae40b18ed92b0a1f92f
nerovalerius <crypto@mailbox.org>
2020-04-09 09:24:04 +0200

    changed name to registration_3d

--- launch/start_everything.sh
+++ launch/start_everything.sh
@@ -6,18 +6,18 @@ echo "Starting Both 3D Cams, Alignment and RVIZ"
 sleep 1
 
 echo "Starting 3D Cams"
-gnome-terminal -e "roslaunch perception start_3d_cams.launch"
+gnome-terminal -e "roslaunch registration_3d start_3d_cams.launch"
 sleep 3
 
 echo "Starting ICP Alignment"
-gnome-terminal -e "bash -c 'rosrun perception preprocess_align_publish allsteps=true algorithm=nlicp /cam_1/depth/color/points /cam_2/depth/color/points'"
+gnome-terminal -e "bash -c 'rosrun registration_3d preprocess_align_publish allsteps=true algorithm=nlicp /cam_1/depth/color/points /cam_2/depth/color/points'"
 sleep 1
 
 echo "Aligning second 3D Cam to Desk"
 gnome-terminal -e "rosrun tf static_transform_publisher 0.5 -1.0 -1.6 -1.0 -0.1 0.0 3.0 world_frame cam_2_depth_optical_frame 3"
 
 echo "Starting RVIZ"
-gnome-terminal -e "bash -c 'roslaunch perception rviz_with_both_cams.launch'"
+gnome-terminal -e "bash -c 'roslaunch registration_3d rviz_with_both_cams.launch'"
 sleep 1
 
 echo "Finished"
